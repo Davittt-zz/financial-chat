c44a2d307416735b402d0ded044b59e0d8fd7756
ddonari <ddonari@gmail.com>
2019-10-24 11:08:05 -0300

    Add GetUser method in IDatabaseProvider

--- JobsityFinancialChat.Providers/IDatabaseProvider.cs
+++ JobsityFinancialChat.Providers/IDatabaseProvider.cs
@@ -1,9 +1,17 @@
-﻿using System.Threading.Tasks;
+﻿using JobsityFinancialChat.Domain.Models.DB;
+using System.Threading.Tasks;
 
 namespace JobsityFinancialChat.Providers
 {
     public interface IDatabaseProvider
     {
+        #region User
+        Task<ApplicationUser> GetUser(string email);
+
+
+        #endregion User
+
+
         #region General
 
         Task Save();
