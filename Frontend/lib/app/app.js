786d048de2a824a23febceb5a37c16ce514c3cdb
ddonari <ddonari@gmail.com>
2019-10-25 05:09:58 -0300

    Add Frontend

---
+++ Frontend/lib/app/app.js
@@ -0,0 +1,15 @@
+﻿
+function appendLine(message, name) {
+
+    let nameElement = document.createElement('strong');
+    let msgElement = document.createElement('em');
+
+    nameElement.innerText = `User:` + name + `: ` + JSON.stringify(message.text);
+    msgElement.innerText = ` SendDate:` + JSON.stringify(message.sendDate);
+
+    let li = document.createElement('li');
+    li.appendChild(nameElement);
+    li.appendChild(msgElement);
+
+    $('#messages').append(li);
+};
